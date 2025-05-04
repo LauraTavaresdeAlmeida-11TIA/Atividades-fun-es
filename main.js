66688a91045df5acc7dbd4a5a54683e84cf82b79
Laura Tavares <laura.tavares.almeida19@gmail.com>
2025-05-04 20:32:33 -0300

    Segundo commit com os 6 exemplos

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("Seja bem-vindo(a)!")
 console.log("É um prazer tê-lo aqui.")
 console.log("Aproveite")
 }
-
+mensagem()
 
 // Exemplo 2 Função com parâmetros
 function somarNumeros(valor1, valor2) {
@@ -22,4 +22,35 @@ function boasVindas(nome, cargo) {
 
 boasVindas("Marcio", "professor")
 
-// Exemplo 4 
+// Exemplo 4 Função com Return
+function somar(valor1, valor2) {
+    return valor1 + valor2
+}
+
+let resultado = somar(29, 32)
+console.log(somar(12, 12))
+
+console.log(resultado)
+
+// Exemplo 5 Função com return
+function boasVindas(nome, cargo) {
+    return 'Seja bem-vindo(a) ${cargo} ${noma}!'
+}
+
+let mensagem = boasVindas("Marcio", "professor")
+
+console.log(mensagem)
+
+// Exemplo 6 Função condicional sem else
+function parImpar(valor) {
+    if (valor % 2 === 0) {
+        return "par"
+    } else {
+        return "impar"
+    }
+}
+
+let verificao = parImpar(13)
+
+console.log9(verificao)
+
