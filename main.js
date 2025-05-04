0cb3e859209c535bc5f82dff614a65c79bf7486d
Laura Tavares <laura.tavares.almeida19@gmail.com>
2025-05-04 20:17:00 -0300

    Primeiro commit html

---
+++ main.js
@@ -0,0 +1,25 @@
+// Exemplo 1
+function mensagem() {
+console.log("Seja bem-vindo(a)!")
+console.log("É um prazer tê-lo aqui.")
+console.log("Aproveite")
+}
+
+
+// Exemplo 2 Função com parâmetros
+function somarNumeros(valor1, valor2) {
+    let soma = valor1 + valor2
+    console.log(soma)
+}
+
+console.log(soma)
+somarNumeros(15, 27)
+
+// Exemplo 3 Função com parâmetros
+function boasVindas(nome, cargo) {
+    console.log('Seja bem-vindo(a) ${cargo} ${nome}!')
+}
+
+boasVindas("Marcio", "professor")
+
+// Exemplo 4 
